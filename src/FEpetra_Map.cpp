449537bacc779deb098b988d026ddb2bffc32910
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-01-23 22:02:46 +0000

    Summary: Overdue misc checkin
    Bugs: None filed
    
    
    Specific options:
    -----------------
    
    (*) cmake/TPLs/FindTPLMPI.cmake: Don't need this file anymore
    
    (*) cmake/package_arch/PackageArchGlobalMacros.cmake: Shortening
    documenation strings so that they fit in the GUI.
    
    (*) cmake/package_arch/PackageArchSetupMPI.cmake: Fixing output typos.
    
    (*) packages/CTrilinos/src/FEpetra_Map.cpp: Adding whitespace
    
    (*) packages/epetraext/cmake/EpetraExt_config.h.in: Cleaning out some
    junk.
    
    (*) Thyra_NonlinearSolverBuilderBase.hpp,
    Thyra_DefaultNonlinearSolverBuilder.hpp, thyra/test/CMakeLists.txt,
    thyra/test/nonlinear/CMakeLists.txt,
    thyra/test/nonlinear/UnitTests/CMakeLists.txt,
    DefaultNonlinearSolverBuilder_UnitTests.cpp: Skeleton for new
    nonlinear solver builder interface, default class and unit testing.
    
    (*) packages/phalanx/src/Phalanx_Allocator_Contiguous.hpp: Fixed code
    that was passing in a zero size to arcp<T>(size) which is now illegal.
    
    (*) sampleScripts/linux-godel-gcc-4.1.2-mpi-opt-cmake: Updating for
    new MPI setup.
    
    
    Tested on godel.sandia.gov with:
    --------------------------------
    
    - CMake serial debug rebuild (all but 1 of the 306 tests ran passed,
    see below)
    
    - CMake mpi release rebuild (all 280 tests ran passed)
    
    
    
    The following tests FAILED with the serial debug build:
    
             19 - Teuchos_Containers_test (SEGFAULT)
    
    The test Teuchos_Containers_test has real memory problems (see bug
    4181 going back to 2008/09/16).  We need to get rid of this code
    because no-one wants to maintain it and it is duplicate code.

--- src/FEpetra_Map.cpp
+++ src/FEpetra_Map.cpp
@@ -60,6 +60,7 @@ int Epetra_Map_NumGlobalElements( MapID mapID )
   return FEpetra::getMap(mapID)->NumGlobalElements();
 }
 
+
 int Epetra_Map_NumMyElements( MapID mapID )
 {
   return FEpetra::getMap(mapID)->NumMyElements();
