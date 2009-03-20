029d94f3882ca25caf1d754bb406fad7a3c34b6d
Damian Rouson <rouson@sandia.gov>
2009-03-20 19:53:33 +0000

    SUMMARY:   [Committing code for testing g++ 'enum' compilation error]
         BUGS:      [see above]
    
         CHECKLIST:
    
         1)   Did you add any source files?  [no]
         1.a) Did you do a 'cvs add'?  [no]
         1.b) Did you add them to a Makefile.am file in some way?  [???]
         1.c) Did you do a new bootstrap?  [yes]
    
         2)   Did you update or change the build system?  [no]
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  [N.A.]
    
         COMMENTS:
    
         []
    
    (checklist version 2007-06-20)

--- src/FEpetra_Map.h
+++ src/FEpetra_Map.h
@@ -1,29 +1,21 @@
 /* True C header file! */
-
-
 #ifndef FEPETRA_MAP_H
 #define FEPETRA_MAP_H
-
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+  typedef int MapID;
 
+  MapID Epetra_Map_Create( int numGlobalElements );
 
-typedef int MapID;
+  void Epetra_Map_Destroy( MapID mapID );
 
-MapID Epetra_Map_Create( int numGlobalElements );
+  int Epetra_Map_NumGlobalElements( MapID mapID );
 
-void Epetra_Map_Destroy( MapID mapID );
-
-int Epetra_Map_NumGlobalElements( MapID mapID );
-
-int Epetra_Map_NumMyElements( MapID mapID );
-
-
+  int Epetra_Map_NumMyElements( MapID mapID );
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
-
 
 #endif /* FEPETRA_MAP_H */
--- src/FEpetra_Map_Cpp.hpp
+++ src/FEpetra_Map_Cpp.hpp
@@ -1,24 +1,15 @@
-#include "FEpetra_Map.h"
-#include "Teuchos_RCP.hpp"
-
-
 #ifndef FEPETRA_MAP_CPP_HPP
 #define FEPETRA_MAP_CPP_HPP
 
+#include "FEpetra_Map.h"
+#include "Teuchos_RCP.hpp"
 
 class Epetra_Map;
 
-
 namespace FEpetra {
+  using Teuchos::RCP;
 
-
-using Teuchos::RCP;
-
-
-const RCP<const Epetra_Map> getMap( MapID mapID );
-
-
+  const RCP<const Epetra_Map> getMap( MapID mapID );
 } // namespace FEpetra
-
 
 #endif // FEPETRA_MAP_CPP_HPP
